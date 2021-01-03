c269dd4c856bb8626e603247093b2b4c1d173363
Spinnafre <davispenha@gmail.com>
2021-01-03 10:02:57 -0300

    add

---
+++ server.js
@@ -0,0 +1,37 @@
+const express=require('express')
+const app=express()
+const path=require('path')
+
+const server=require('http').createServer(app)
+const io=require('socket.io')(server)
+
+app.use(express.static(path.join(__dirname,'public')))
+
+app.set('views',path.join(__dirname,'public'))
+// Use esse método para engines não fornecidas ou usar uma extensao diferent 
+// Para arquivos HTML irá chamar a função ejs.renderFile, para rendelizálos
+app.engine('html',require('ejs').renderFile)
+// Aviso ao express qual template irei usar, o template irá se responsabilizar por arquivos .html
+app.set('view engine','html')
+
+
+app.use('/',(req,res)=>{
+    res.render('index.html')
+})
+
+let messages=[]
+// Quando conectar ao socket irá realizar determinadas ações
+io.on('connection',socket=>{
+    console.log('SOCKET CONECTADO =',socket.id)
+
+    socket.emit('previousMessage',messages)
+
+    socket.on('sendMessage',data=>{
+        messages.push(data)
+        // Irá emitir para todos as portas conectadas
+        socket.broadcast.emit('receivedMessage',data)
+    })
+})
+
+
+server.listen(3000)
